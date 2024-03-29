20b4be2b31ae0ec7bbfcef8588ae19595590e42d
Guilherme Araujo <82453465+GuiMatheus1313@users.noreply.github.com>
2024-03-29 15:26:31 +0000

    Funcionamento simples de busca binária

--- main.c
+++ main.c
@@ -1,10 +1,36 @@
 #include <stdio.h>
+#include <stdbool.h> //Colocar valores de bool em C
 
 int main(void) {
-  int vetor_busca = {4,7,2,1,3,0,6,8,9};
-  int tam = 9, chave;
+  int vetor_busca[] = {5,15,25,30,40,65,100,145};
+  int fim = 8, chave, comeco = 0, meio;
+  bool achou = false;
 
-  chave = 65;
+  printf("Digite a chave que deseja procurar: ");
+  scanf("%d", &chave);
+
+  meio = (comeco+fim)/2;
+
+  while(achou == false && comeco <= fim)
+  {
+    if(vetor_busca[meio] == chave)
+    {
+      achou = true;
+      break;
+    }
+    else{
+      if(chave < vetor_busca[meio])
+        fim = meio - 1;
+      else
+        comeco = meio + 1;
+    }
+    meio = (comeco+fim)/2;
+  }
+  if(achou == true)
+    printf("Encontrado na posicao: %d", meio);
+  else
+    printf("Não foi encontrado");
+  
 
   
   
